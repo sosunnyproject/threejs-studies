51159fbe9fb455ce9d36df354135a52d1cb0a6a5
sosunnyproject <parksosun1103@gmail.com>
2021-09-26 22:55:02 +0900

    setup fps

--- fps/render.js
+++ fps/render.js
@@ -1,10 +1,35 @@
 // https://threejsfundamentals.org/threejs/lessons/threejs-cameras.html
 import * as THREE from '../resources/threejs/three.module.js';
 import { GUI } from '../resources/dat.gui.module.js';
+import { WEBGL } from '../resources/WebGL.js';
+import Stats from '../resources/stats.module.js';
 import { OrbitControls } from '../resources/OrbitControls.js';
+import { FirstPersonControls } from '../resources/FirstPersonControls.js';
 
 function main() {
+  var clock = new THREE.Clock();
 
+  // create a scene, that will hold all our elements such as objects, cameras and lights.
+  var scene = new THREE.Scene();
+
+  // create a camera, which defines where we're looking at.
+  var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
+  // create a render and set the size
+  var webGLRenderer = new THREE.WebGLRenderer();
+  webGLRenderer.setClearColor(new THREE.Color(0x000, 1.0));
+  webGLRenderer.setSize(window.innerWidth, window.innerHeight);
+  webGLRenderer.shadowMapEnabled = true;
+
+  // position and point the camera to the center of the scene
+  camera.position.x = 100;
+  camera.position.y = 10;
+  camera.position.z = 10;
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+
+  var camControls = new FirstPersonControls(camera);
 }
 
 if(!WEBGL.isWebGLAvailable()) {
