41b703373f10b09086032b10e1f3be8ccf5e1124
sosunnyproject <parksosun1103@gmail.com>
2021-09-29 22:22:55 +0900

    update scene

--- fps/render.js
+++ fps/render.js
@@ -6,6 +6,22 @@ import Stats from '../resources/stats.module.js';
 import { OrbitControls } from '../resources/OrbitControls.js';
 import { FirstPersonControls } from '../resources/FirstPersonControls.js';
 
+const treeParams = {
+  radius: 5,
+  detail: 1,
+  cameraZ: 10,
+  xpos: 20,
+  ypos: 10,
+  color: '#00ff00'
+}
+
+const params = {
+  fov: 20,
+  aspect: 2, 
+  zNear: 0.1,
+  zFar: 1000
+}
+
 function main() {
   var clock = new THREE.Clock();
 
@@ -14,28 +30,32 @@ function main() {
   scene.background = new THREE.Color(0xAAAAAA)
 
   // create a camera, which defines where we're looking at.
-  var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  function makeCamera() {
+    const { fov, aspect, zNear, zFar} = params;  // the canvas default
+    return new THREE.PerspectiveCamera(fov, aspect, zNear, zFar);
+  }
+  const camera = makeCamera();
+  camera.position.set(8, 4, 10).multiplyScalar(3);
+  camera.lookAt(0, 0, 0);
 
   // create a render and set the size
   const canvas = document.querySelector('#c');
-  var webGLRenderer = new THREE.WebGLRenderer({ canvas });
-  webGLRenderer.setClearColor(new THREE.Color(0x000, 1.0));
-  webGLRenderer.setSize(window.innerWidth, window.innerHeight);
-  webGLRenderer.shadowMap.enabled = true;
+  var renderer = new THREE.WebGLRenderer({ canvas });
+  renderer.setClearColor(new THREE.Color(0x000, 1.0));
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
 
   var axes = new THREE.AxesHelper(20);
   scene.add(axes);
 
-  var planeGeometry = new THREE.PlaneGeometry(60, 60, 1, 1);
-  var planeMat = new THREE.MeshBasicMaterial({ color: 0xccccc });
-  var plane = new THREE.Mesh(planeGeometry, planeMat);
+  var groundGeometry = new THREE.PlaneGeometry(60, 60, 1, 1);
+  var groundMaterial = new THREE.MeshBasicMaterial({ color: 0xccccc });
+  var groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
 
-  plane.rotation.x = -0.25*Math.PI;
-  plane.position.x = 20;
-  plane.position.y = 0;
-  plane.position.z = 0;
+  groundMesh.rotation.x = -0.5*Math.PI;
+  groundMesh.receiveShadow = true;
+  scene.add(groundMesh)
 
-  scene.add(plane)
   // position and point the camera to the center of the scene
   camera.position.x = 100;
   camera.position.y = 10;
@@ -43,11 +63,43 @@ function main() {
   camera.lookAt(new THREE.Vector3(0, 0, 0));
 
   // var camControls = new FirstPersonControls(camera);
+  
+  // orbit controls
+  const controls = new OrbitControls( camera, renderer.domElement);
+  controls.enableZoom = true;
+  controls.enableDamping = true;
+  controls.update();
+
+  // tree object
+  const tree = new THREE.Object3D();
+  scene.add(tree);
+  
+  const grassGeometry = new THREE.DodecahedronGeometry(treeParams.radius, Math.floor(treeParams.detail))
+  const grassMaterial = new THREE.MeshBasicMaterial( { color: treeParams.color } );
+  const grassMesh = new THREE.Mesh( grassGeometry, grassMaterial );
+  grassMesh.position.x = treeParams.xpos
+  grassMesh.position.y = treeParams.ypos
+  tree.add(grassMesh)
+
+  const trunkGeometry = new THREE.CylinderGeometry(2, 2, 6, 12)
+  const trunkMaterial = new THREE.MeshBasicMaterial({ color: "brown"})
+  const trunkMesh = new THREE.Mesh( trunkGeometry, trunkMaterial );
+  trunkMesh.position.x = treeParams.xpos
+  trunkMesh.position.y = treeParams.ypos - 5
+  tree.add(trunkMesh);
+
+  // camera gui
+  const gui = new GUI();
+  const guiBox = gui.addFolder('guiBox');
+  guiBox.add(params, 'fov', 1, 100).onChange(makeCamera)
+  guiBox.add(params, 'aspect', 1, 20).onChange(makeCamera)
+  guiBox.add(params, 'zNear', 0.1, 1).onChange(makeCamera)
+  guiBox.add(params, 'zFar', 500, 2000).onChange(makeCamera)
 
   const animate = function () {
     requestAnimationFrame( animate );
 
-    webGLRenderer.render( scene, camera );
+    renderer.render( scene, camera );
   };
 
   animate()
@@ -58,6 +110,7 @@ if(!WEBGL.isWebGLAvailable()) {
 	 document.getElementById( 'container' ).appendChild( warning );
 } else {
   main();
+  initStats();
 }
 
 function initStats() {
@@ -66,6 +119,6 @@ function initStats() {
   stats.domElement.style.position = 'absolute';
   stats.domElement.style.left = '0px';
   stats.domElement.style.top = '0px';
-  document.body.getElementById("stats-output").append(stats.domElement);
+  document.querySelector("#stats-output").append(stats.domElement);
   return stats;
 }
