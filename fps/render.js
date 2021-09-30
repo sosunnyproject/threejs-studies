acfbb77721b751eb7fa4d37b7a5c73cb10cd87a8
sosunnyproject <parksosun1103@gmail.com>
2021-09-30 21:43:47 +0900

    update colorful trees

--- fps/render.js
+++ fps/render.js
@@ -7,20 +7,21 @@ import { OrbitControls } from '../resources/OrbitControls.js';
 import { FirstPersonControls } from '../resources/FirstPersonControls.js';
 
 const treeParams = {
-  radius: 5,
-  detail: 1,
-  cameraZ: 10,
+  radius: 7,
+  detail: 5,
   xpos: 20,
-  ypos: 10,
+  ypos: 11,
   color: '#00ff00'
 }
 
 const params = {
   fov: 20,
-  aspect: 2, 
-  zNear: 0.1,
+  aspect: 1.5, 
+  zNear: 5,
   zFar: 1000
 }
+
+const WIDTH = window.innerWidth, HEIGHT = window.innerHeight
 
 function main() {
   var clock = new THREE.Clock();
@@ -35,21 +36,21 @@ function main() {
     return new THREE.PerspectiveCamera(fov, aspect, zNear, zFar);
   }
   const camera = makeCamera();
-  camera.position.set(8, 4, 10).multiplyScalar(3);
+  camera.position.set(8, 4, 10).multiplyScalar(1);
   camera.lookAt(0, 0, 0);
 
   // create a render and set the size
   const canvas = document.querySelector('#c');
   var renderer = new THREE.WebGLRenderer({ canvas });
   renderer.setClearColor(new THREE.Color(0x000, 1.0));
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(WIDTH, HEIGHT);
   renderer.shadowMap.enabled = true;
 
   var axes = new THREE.AxesHelper(20);
   scene.add(axes);
 
-  var groundGeometry = new THREE.PlaneGeometry(60, 60, 1, 1);
-  var groundMaterial = new THREE.MeshBasicMaterial({ color: 0xccccc });
+  var groundGeometry = new THREE.PlaneGeometry(100, 100, 1, 1);
+  var groundMaterial = new THREE.MeshBasicMaterial({ color: 0xccccc, side: THREE.DoubleSide });
   var groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
 
   groundMesh.rotation.x = -0.5*Math.PI;
@@ -71,22 +72,11 @@ function main() {
   controls.update();
 
   // tree object
-  const tree = new THREE.Object3D();
-  scene.add(tree);
-  
-  const grassGeometry = new THREE.DodecahedronGeometry(treeParams.radius, Math.floor(treeParams.detail))
-  const grassMaterial = new THREE.MeshBasicMaterial( { color: treeParams.color } );
-  const grassMesh = new THREE.Mesh( grassGeometry, grassMaterial );
-  grassMesh.position.x = treeParams.xpos
-  grassMesh.position.y = treeParams.ypos
-  tree.add(grassMesh)
-
-  const trunkGeometry = new THREE.CylinderGeometry(2, 2, 6, 12)
-  const trunkMaterial = new THREE.MeshBasicMaterial({ color: "brown"})
-  const trunkMesh = new THREE.Mesh( trunkGeometry, trunkMaterial );
-  trunkMesh.position.x = treeParams.xpos
-  trunkMesh.position.y = treeParams.ypos - 5
-  tree.add(trunkMesh);
+  for(let i = 0; i < 100; i++){
+    const x = getRandomArbitrary(-200, 200)
+    const tree = makeTree(x, 15, getRandomArbitrary(-100, 100))
+    scene.add(tree);  
+  }
 
   // camera gui
   const gui = new GUI();
@@ -105,6 +95,33 @@ function main() {
   animate()
 }
 
+function makeTree(xpos, ypos, zpos) {
+
+  const tree = new THREE.Object3D();
+  
+  const grassColors = ["rgb(227, 101, 91)", "rgb(220, 214, 247)", "rgb(217, 237, 146)", "rgb(181,228,140)", "rgb(153,217,140)", "rgb(118,200,147)", "rgb(82,182,154)", "rgb(52,160,164)"]
+  const grassInd = getRandomInt(0, grassColors.length)
+  const grassGeometry = new THREE.DodecahedronGeometry(getRandomArbitrary(4.0, 10.0), getRandomInt(0, 3))
+  const grassMaterial = new THREE.MeshBasicMaterial( { color: grassColors[grassInd] } );
+  const grassMesh = new THREE.Mesh( grassGeometry, grassMaterial );
+  grassMesh.position.x = xpos
+  grassMesh.position.y = ypos
+  grassMesh.position.z = zpos
+  tree.add(grassMesh)
+
+  const trunkColors = [ "rgb(232, 174, 183)", "rgb(115, 72, 48)", "rgb(94, 116, 127)", "rgb(197, 152, 73)", "rgb(156, 179, 128)" ]
+  const colorIndex = getRandomInt(0, trunkColors.length)
+  const trunkGeometry = new THREE.CylinderGeometry(1, 2, 6, 12)
+  const trunkMaterial = new THREE.MeshBasicMaterial({ color: trunkColors[colorIndex] })
+  const trunkMesh = new THREE.Mesh( trunkGeometry, trunkMaterial );
+  trunkMesh.position.x = xpos
+  trunkMesh.position.y = ypos - 10
+  trunkMesh.position.z = zpos
+  tree.add(trunkMesh);
+
+  return tree
+}
+
 if(!WEBGL.isWebGLAvailable()) {
   const warning = WEBGL.getWebGLErrorMessage();
 	 document.getElementById( 'container' ).appendChild( warning );
@@ -121,4 +138,14 @@ function initStats() {
   stats.domElement.style.top = '0px';
   document.querySelector("#stats-output").append(stats.domElement);
   return stats;
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
